f6354de6677e886414869c8f046b1d41856484b6
Chr1stian234 <s3892544@student.rmit.edu.au>
2024-08-19 19:53:21 +1000

    Fixed password utils await function

--- src/utils/PasswordUtil.ts
+++ src/utils/PasswordUtil.ts
@@ -2,7 +2,7 @@ import argon2 from 'argon2';
 
 export const hashPassword = async (password: string) => {
     try {
-        return await argon2.hash(password);
+        return argon2.hash(password); 
     } catch (error: unknown) {
         throw new Error('Error hashing password');
     }
@@ -10,7 +10,7 @@ export const hashPassword = async (password: string) => {
 
 export const verifyPassword = async (hashedPassword: string, password: string) => {
     try {
-        return await argon2.verify(hashedPassword, password);
+        return argon2.verify(hashedPassword, password);
     } catch (error: unknown) {
         throw new Error('Error verifying password');
     }
